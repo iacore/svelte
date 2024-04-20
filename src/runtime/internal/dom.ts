7936f2efcd1d8ad168ace19f47fc6d011b628fce
iacore <noreply+gpg-stub@1a-insec.net>
2024-04-20 10:49:03 +0000

    patch

--- src/runtime/internal/dom.ts
+++ src/runtime/internal/dom.ts
@@ -582,47 +582,11 @@ export function is_crossorigin() {
 	return crossorigin;
 }
 
+import { addResizeListener, removeResizeListener } from "./resize_listener"
 export function add_resize_listener(node: HTMLElement, fn: () => void) {
-	const computed_style = getComputedStyle(node);
-
-	if (computed_style.position === 'static') {
-		node.style.position = 'relative';
-	}
-
-	const iframe = element('iframe');
-	iframe.setAttribute('style',
-		'display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; ' +
-		'overflow: hidden; border: 0; opacity: 0; pointer-events: none; z-index: -1;'
-	);
-	iframe.setAttribute('aria-hidden', 'true');
-	iframe.tabIndex = -1;
-
-	const crossorigin = is_crossorigin();
-
-	let unsubscribe: () => void;
-
-	if (crossorigin) {
-		iframe.src = "data:text/html,<script>onresize=function(){parent.postMessage(0,'*')}</script>";
-		unsubscribe = listen(window, 'message', (event: MessageEvent) => {
-			if (event.source === iframe.contentWindow) fn();
-		});
-	} else {
-		iframe.src = 'about:blank';
-		iframe.onload = () => {
-			unsubscribe = listen(iframe.contentWindow, 'resize', fn);
-		};
-	}
-
-	append(node, iframe);
-
+	addResizeListener(node, fn)
 	return () => {
-		if (crossorigin) {
-			unsubscribe();
-		} else if (unsubscribe && iframe.contentWindow) {
-			unsubscribe();
-		}
-
-		detach(iframe);
+		removeResizeListener(node, fn)
 	};
 }
 
